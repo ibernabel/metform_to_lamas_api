4330810624c2c12f923111b7203a99414b68fd7a
ibernabel <id.bernabel@gmail.com>
2025-05-04 21:30:52 -0400

    Bump version to 1.3.2 and update vehicle handling in customer submission

--- metform-to-lamas-api.php
+++ metform-to-lamas-api.php
@@ -4,7 +4,7 @@
  * Plugin Name:       MetForm to Lamas API Integration
  * Plugin URI:        https://github.com/ibernabel/metform_to_lamas_api
  * Description:       Sends MetForm submission data to specified Laravel API endpoints using Action Scheduler. Handles different logic based on form ID.
- * Version:           1.3.1
+ * Version:           1.3.2
  * Requires at least: 5.2
  * Requires PHP:      8.0
  * Author:            Idequel Bernabel
@@ -371,7 +371,7 @@ function _mfla_process_full_customer_submission($form_submission_data)
         'phones' => [],
         'addresses' => [],
       ],
-      'vehicles' => [],
+      'vehicle' => [],
       'references' => [],
     ],
     'details' => [],
@@ -408,19 +408,19 @@ function _mfla_process_full_customer_submission($form_submission_data)
   if ($street) $addresses[] = ['street' => $street, 'type' => 'home'];
   if (!empty($addresses)) $api_payload['customer']['details']['addresses'] = $addresses;
 
-  // Customer Vehicles
-  $vehicles = [];
+  // Customer Vehicle
+  $vehicle = [];
   $vehicle_data = [];
   $vehicle_data['is_owned'] = $get_value('vehiculo-propio', null, null, 'mfla_to_bool');
   $vehicle_data['is_financed'] = $get_value('vehiculo-financiado', null, null, 'mfla_to_bool');
   $vehicle_data['brand'] = $get_value('vehiculo-marca');
   $vehicle_data['year'] = $get_value('vehiculo-anno', null, null, 'mfla_to_numeric');
   if ($vehicle_data['brand'] !== null || $vehicle_data['year'] !== null || $vehicle_data['is_owned'] !== null || $vehicle_data['is_financed'] !== null) {
-    $vehicles[] = array_filter($vehicle_data, function ($value) {
+    $vehicle[] = array_filter($vehicle_data, function ($value) {
       return $value !== null;
     });
   }
-  if (!empty($vehicles)) $api_payload['customer']['vehicles'] = $vehicles;
+  if (!empty($vehicle)) $api_payload['customer']['vehicle'] = $vehicle;
 
   // Customer References
   $references = [];
