a5c92e8ed6382e2de8bd6c321f7dca99fbbc16ba
ibernabel <id.bernabel@gmail.com>
2025-05-04 21:45:56 -0400

    Bump version to 1.3.3 and update vehicle data keys for clarity in submission processing

--- metform-to-lamas-api.php
+++ metform-to-lamas-api.php
@@ -4,7 +4,7 @@
  * Plugin Name:       MetForm to Lamas API Integration
  * Plugin URI:        https://github.com/ibernabel/metform_to_lamas_api
  * Description:       Sends MetForm submission data to specified Laravel API endpoints using Action Scheduler. Handles different logic based on form ID.
- * Version:           1.3.2
+ * Version:           1.3.3
  * Requires at least: 5.2
  * Requires PHP:      8.0
  * Author:            Idequel Bernabel
@@ -413,9 +413,9 @@ function _mfla_process_full_customer_submission($form_submission_data)
   $vehicle_data = [];
   $vehicle_data['is_owned'] = $get_value('vehiculo-propio', null, null, 'mfla_to_bool');
   $vehicle_data['is_financed'] = $get_value('vehiculo-financiado', null, null, 'mfla_to_bool');
-  $vehicle_data['brand'] = $get_value('vehiculo-marca');
-  $vehicle_data['year'] = $get_value('vehiculo-anno', null, null, 'mfla_to_numeric');
-  if ($vehicle_data['brand'] !== null || $vehicle_data['year'] !== null || $vehicle_data['is_owned'] !== null || $vehicle_data['is_financed'] !== null) {
+  $vehicle_data['vehicle_brand'] = $get_value('vehiculo-marca');
+  $vehicle_data['vehicle_year'] = $get_value('vehiculo-anno', null, null, 'mfla_to_numeric');
+  if ($vehicle_data['vehicle_brand'] !== null || $vehicle_data['vehicle_ year'] !== null || $vehicle_data['is_owned'] !== null || $vehicle_data['is_financed'] !== null) {
     $vehicle[] = array_filter($vehicle_data, function ($value) {
       return $value !== null;
     });
